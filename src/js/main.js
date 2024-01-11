4bddf452a0f75725ac680dd8580abf631016a7c4
YariMorcus <yarimorcus@zeelandnet.nl>
2024-01-11 17:03:53 +0100

    add dropdown menu functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,33 @@ class Navigation {
   #body = document.body;
   #toggleButton = document.querySelector('#js-nav__toggle');
   #navMenu = document.querySelector('#js-nav__menu');
+  #navMenuLinks = document.querySelectorAll('.nav__menu-link');
 
   constructor() {
-    // Check if JS is available. If yes, add .js to <body>
-    // If not available, return prematurely to prevent errors
+    /*
+      Check if JS is available. If yes, add .js to <body>
+      If not available, return prematurely to prevent errors
+    */
     if (!this.#addJSclass()) return;
 
-    // Listen for click event on toggle button
+    /*
+      Listen for click event on toggle button to
+      deliver mobile navigation toggle functionality
+    */
     this.#toggleButton.addEventListener(
       'click',
       this.#navController.bind(this)
     );
+
+    // Deliver dropdown functionality for specific menu items
+    this.#navMenuLinks.forEach(navMenuLink => {
+      navMenuLink.addEventListener('click', e => {
+        const dropdown = e.target.classList.contains('is-dropdown');
+
+        if (!dropdown) return;
+        this.#dropdownController(navMenuLink);
+      });
+    });
   }
 
   /**
@@ -35,6 +51,14 @@ class Navigation {
     // Show mobile menu
     this.#navMenu.classList.toggle('is-active');
     this.#toggleButton.classList.toggle('menu-expanded');
+  }
+
+  /**
+   * Deliver dropdown functionality for specific menu items
+   * @returns {undefined}
+   */
+  #dropdownController(curDropdownLink) {
+    curDropdownLink.classList.toggle('is-dropdown--expanded');
   }
 }
 
